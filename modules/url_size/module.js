b393d2c096fd903eb822cab26a8930dd44f29123
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-06-27 19:30:13 +0300

    New module: URLSize.

---
+++ modules/url_size/module.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var request = require('request')
+  , async = require('async')
+  , format = require('util').format
+  , c = require('irc').colors.wrap;
+
+function URLSize(io, config) {
+  this.io = io;
+  this.config = config;
+}
+
+function checkURL(info, cb) {
+  var size = this.config.warnSize * 1024 * 1024;
+  if (!info.matches) { return; }
+  request.head(info.matches[0], function (err, res, body) {
+    if (!err && res.headers && res.headers['content-length'] > size) { cb(res.headers); }
+  });
+}
+
+function showWarning(i) {
+  var size = i['content-length'] / 1024 / 1024;
+  return format('Warning - Size: %s - Type: %s',
+    c('light_red', size.toFixed(1) + ' MB'), i['content-type']);
+}
+
+URLSize.prototype.routes = {
+  'url': {
+        route: /(?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/i,
+         help: 'an route to match any urls',
+      handler: checkURL,
+    formatter: showWarning
+  }
+};
+
+
+module.exports = {
+         name: 'URLSize',
+  description: 'looks for links in messages and warns about big downloads',
+       author: 'Ville "tuhoojabotti" Lahdenvuo',
+      contact: 'tuhoojabotti at gmail or tuhoojabotti@IRCNet',
+      version: '0.1',
+       module: URLSize
+};
