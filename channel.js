6486ad35025d4073cdabf4c5b876e5aa6d83ab7a
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-07-11 20:16:36 +0300

    Fixed operator regex wildcard.

--- channel.js
+++ channel.js
@@ -32,7 +32,7 @@ Channel.prototype.init = function () {
 
   if (this.config.operators) {
     this.operators = this.config.operators.map(function (op) {
-      return new RegExp(op.replace(/[-[\]{}()+?.,\\^$|#\s]/g, "\\$&").replace(/\*/g, '(.+?)')); });
+      return new RegExp(op.replace(/[-[\]{}()+?.,\\^$|#\s]/g, "\\$&").replace(/\*/g, '(.*?)')); });
   }
 };
 
