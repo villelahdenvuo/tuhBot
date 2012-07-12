aae789799fefced008aade7a808cd9033613676d
Vesa Laakso <laakso.vesa@gmail.com>
2012-07-12 13:17:16 +0300

    Better initializing of url_title module

--- modules/url_title/module.js
+++ modules/url_title/module.js
@@ -7,32 +7,45 @@ var URLTitle = {
   description: 'looks for links in messages and spews out information about them',
        author: 'Ville "tuhoojabotti" Lahdenvuo',
       contact: 'tuhoojabotti at gmail or IRCNet',
-      version: '1.3.0',
+      version: '1.3.1',
          init: init
 };
 
 function init() {
-  this.context.handlers = fs.readdirSync(__dirname + '/handlers/');
+  var handlerFiles = fs.readdirSync(__dirname + '/handlers/');
+
+  this.context.handlers = {};
+  handlerFiles.forEach(function function_name (file, i) {
+    var jsLoc = file.lastIndexOf('.js');
+    if (jsLoc !== -1) {
+      file = file.substr(0, jsLoc);
+      this.context.handlers[file] = require(__dirname + '/handlers/' + file);
+    }
+  }, this);
 }
 
 function handler(info, cb) {
   var url = info.matches[0]
-    , context = this;
-  for (var i = 0; i < this.handlers.length; i++) {
-    var handlerName = this.handlers[i]
-      , handler = require(__dirname + '/handlers/' + handlerName)
+    , context = this
+    , handlerNames = Object.keys(context.handlers);
+
+  // Loop through every handler and try to match them before
+  // falling back to the generic handler.
+  for (var i = 0; i < handlerNames.length; i++) {
+    var handler = context.handlers[handlerNames[i]]
       , matches = url.match(handler.route);
-    
+
     if (matches) {
       handler.handler.call(context, matches, function (i) {
         cb(handler.formatter.call(context, i));
       });
-      return;
+      return; // We're done here.
     }
   }
+
   // Did not find a special handler, use the generic one.
   var genericHandler = require('./generic');
-  
+
   genericHandler.handler.call(context, url, function (i) {
     cb(genericHandler.formatter.call(context, i));
   });
