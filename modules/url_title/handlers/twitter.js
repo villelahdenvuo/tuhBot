a73c5d12cfeecec2a53e43cf3395b58d1a713739
Vesa Laakso <laakso.vesa@gmail.com>
2012-07-12 03:46:06 +0300

    Better Twitter URLs

--- modules/url_title/handlers/twitter.js
+++ modules/url_title/handlers/twitter.js
@@ -2,17 +2,41 @@
 
 var request = require('request')
   , format = require('util').format
-  , c = require('irc').colors.wrap;
+  , c = require('irc').colors.wrap
+  , async = require('async');
+
+
+function normalizeUrls(text, cb) {
+  var links = text.match(new RegExp('(https?://t\.co/\\w+)', 'g'));
+  if (!links) { cb(text); }
+
+  function normalizeSingleUrl(url, done) {
+    request({url: url, followRedirect: false, method: 'HEAD', timeout: 1500}, function (err, res) {
+      var resolvedUrl = res.headers['location'];
+
+      if (resolvedUrl.length < url.length + 10) {
+        text = text.replace(url, resolvedUrl);
+      }
+      done();
+    });
+  }
+
+  async.forEach(links, normalizeSingleUrl, function (err) {
+    cb(text);
+  });
+}
 
 function handler(matches, cb) {
   request('http://api.twitter.com/1/statuses/show/' + matches[2] + '.json',
     function (err, res, body) {
       if (err || res.statusCode != 200) { return; }
       var data = JSON.parse(body);
-      cb({
-        text: data['text'],
-        user: data['user']['screen_name'],
-        name: data['user']['name']
+      normalizeUrls(data.text, function (text) {
+        cb({
+          text: text,
+          user: data.user.screen_name,
+          name: data.user.name
+        });
       });
     }
   );
